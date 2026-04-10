2229332ebfa9a120e6d39259c192cb2b7f4b20e3
pranavd33 <pranavdeshpande331@gmail.com>
2026-04-10 14:05:33 +0530

    mainpg

--- fe/src/app/app.routes.ts
+++ fe/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Report } from './report/report';
+import { SabhasadMaster } from './sabhasad-master/sabhasad-master';
+import { SabhasadVargani } from './sabhasad-vargani/sabhasad-vargani';
+import { TransactionDetail } from './transaction-detail/transaction-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'report', component: Report },
+  { path: 'sabhasad-master', component: SabhasadMaster },
+  { path: 'sabhasad-vargani', component: SabhasadVargani },
+  { path: 'transaction-detail', component: TransactionDetail },
+];
--- fe/src/app/sabhasad-master/sabhasad-master.ts
+++ fe/src/app/sabhasad-master/sabhasad-master.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sabhasad-master',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './sabhasad-master.html',
   styleUrl: './sabhasad-master.css',
 })
 export class SabhasadMaster {
+  sabhasadForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.sabhasadForm = this.fb.group({
+      name: ['', Validators.required],
+      // id: ['', Validators.required],
+      contactNo: ['', Validators.required],
+      address: ['', Validators.required],
+      date: ['', Validators.required],
+      mailId: [''] // optional
+    });
+  }
+
+  onSubmit() {
+    if (this.sabhasadForm.valid) {
+      console.log(this.sabhasadForm.value);
+      // Handle form submission
+    }
+  }
 }
